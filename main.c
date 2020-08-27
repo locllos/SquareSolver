67b19e1b400e840b29fd83f572b102e4955c0f81
molidex <kakakakaka2018@mail.ru>
2020-08-27 17:54:48 +0300

    Simple example of mipt projects

---
+++ main.c
@@ -0,0 +1,97 @@
+//SquareSolver 
+
+#define INF -1
+
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+int super_pooper_squareSolver(double a, double b, double c, double* pointer_x1, double* pointer_x2)
+{	
+	printf("Trying to solve the square equation:\n(%lg)*x^2 + (%lg)*x + (%lg) = 0\n", a, b, c);
+
+	assert(isfinite(a));
+	assert(isfinite(b));
+	assert(isfinite(c));
+
+	assert(pointer_x1 != NULL);
+	assert(pointer_x2 != NULL);
+	assert(pointer_x2 != pointer_x1);
+
+	if (a == 0) 
+	{ 
+		if (b == 0) 
+		{
+			if (c == 0) return INF;
+			else return 0;
+		}
+		else 
+		{ 
+			(*pointer_x1) = -c/b; 
+
+			return 1; 
+		}
+	}
+
+	else
+	{
+		double discriminant = pow(b, 2) - 4*a*c;
+		if (discriminant < 0) 
+		{
+			return 0;
+		}
+		else if (discriminant == 0) 
+		{ 
+			(*pointer_x1) = (-b)/(2*a);
+
+			return 1;
+		}
+		else 
+		{
+			(*pointer_x1) = (-b + sqrt(discriminant))/(2*a);
+			(*pointer_x2) = (-b - sqrt(discriminant))/(2*a);
+
+			return 2;
+		}
+
+	}
+}
+
+int main()
+{	
+	printf("Welcome! This is SquareSolver programm.\nInput a, b, c coefficients of square equation.\n");
+
+	double a = 0, b = 0, c = 0;
+	scanf("%lg %lg %lg", &a,&b, &c);
+
+	double x1 = 0, x2 = 0;
+	int amount_of_roots = super_pooper_squareSolver(a,b,c, &x1, &x2);
+
+	switch (amount_of_roots)
+	{
+		case(0):
+
+			printf("No roots\n");
+			break;
+		case(1):
+
+			printf("One root:\nx1 = %lg\n", x1);
+			break;
+
+		case(2):
+
+			printf("Two roots:\nx1 = %lg\nx2 = %lg\n", x1,x2);
+			break;
+
+		case(INF):
+
+			printf("Infinite amount of roots\n");
+			break;
+
+		default:
+
+			printf("I do not fucking know what happened.\n");
+			break;
+	}
+
+}
